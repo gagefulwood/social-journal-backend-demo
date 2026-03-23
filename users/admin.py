17ab5882b2dbd297dbe444195d3353aca9936d86
Gage Fulwood <gagetfulwood@gmail.com>
2026-03-22 22:45:55 -0500

    included demo file from milestone one

--- users/admin.py
+++ users/admin.py
@@ -1,3 +1,16 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
+from .models import Users
 
-# Register your models here.
+
+@admin.register(Users)
+class UsersAdmin(UserAdmin):
+    list_display  = ['email', 'username', 'first_name', 'last_name', 'is_mfa_enabled', 'is_active', 'is_staff']
+    search_fields = ['email', 'username', 'first_name', 'last_name']
+    ordering      = ['email']
+
+    fieldsets = UserAdmin.fieldsets + (
+        ('MFA', {'fields': ('is_mfa_enabled', 'mfa_secret')}),
+        ('OAuth', {'fields': ('auth_provider', 'provider_uid')}),
+        ('Contact', {'fields': ('phone_number',)}),
+    )
