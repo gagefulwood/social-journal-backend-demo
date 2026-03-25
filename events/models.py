3d5a6f12e53a4321da2b088770d7a06b95fc8790
Brayden-Mendez <156864054+Brayden-Mendez@users.noreply.github.com>
2026-03-24 20:01:22 -0500

    Feat Events - Brayden
    Implemented events model for issue 10

--- events/models.py
+++ events/models.py
@@ -1,3 +1,22 @@
 from django.db import models
+from users.models import Users
+from contacts.models import Contact
+from lookups.models import ContextCategory
+from django.utils import timezone
 
 # Create your models here.
+class Event(models.Model):
+    user_id = models.ForeignKey(
+        Users,
+        on_delete = models.CASCADE,
+        related_name = 'events',
+    ) 
+    title = models.Charfield(max_length=255)
+    event_timestamp = models.DateTimeField()
+    context_category_id = models.ForeignKey(
+        ContextCategory,
+        null=True,
+        blank=True,
+        on_delete = models.SET_NULL,
+        related_name = 'events',
+    )
