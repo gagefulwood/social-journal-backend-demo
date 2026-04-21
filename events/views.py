a1154c88d91f1dd75a2b288ff7dc0c7dc9f02b0d
Brayden-Mendez <156864054+Brayden-Mendez@users.noreply.github.com>
2026-04-20 21:57:10 -0500

    Brayden - 4/20/26
    Final updates to events views and serializers

--- events/views.py
+++ events/views.py
@@ -1,10 +1,11 @@
-from rest_framework import viewsets, mixins
+from rest_framework.viewsets import ModelViewSet
 from rest_framework.permissions import IsAuthenticated
 from django_filters.rest_framework import DjangoFilterBackend
 
 from .models import Event
-from .serializers import EventSerializer
+from .serializers import EventSerializer, EventListSerializer
 from core.pagination import StandardResultsPagination
+from core.permissions import IsOwner
 
 class EventViewSet(ModelViewSet):
     '''
@@ -15,7 +16,7 @@ class EventViewSet(ModelViewSet):
     Retrieve, update, and delete added in Sprint 3 with full journal entry feature **
     '''
     serializer_class = EventSerializer
-    permission_classes = [IsAuthenticated]
+    permission_classes = [IsAuthenticated, IsOwner]
     pagination_class = StandardResultsPagination
     filter_backends = [DjangoFilterBackend]
 
@@ -28,10 +29,21 @@ class EventViewSet(ModelViewSet):
         "options",
     ]
 
+    def get_serializer_class(self): 
+        if self.action == "list":
+            return EventListSerializer
+        return EventSerializer
+
     def get_queryset(self):
         if getattr(self, 'swagger_fake_view', False):
             return Event.objects.none()
-        return Event.objects.filter(user=self.request.user)
-    
+
+        return (
+            Event.objects
+            .filter(user=self.request.user)
+            .select_related("context_category")
+            .prefetch_related("participants__contact")
+        )
+
     def perform_create(self, serializer):
         serializer.save(user=self.request.user)
